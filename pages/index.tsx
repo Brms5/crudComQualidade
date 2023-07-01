a98e840bbf63b1e888eabcaf0236c637640e3b9c
Bruno <bruno.moreira@meta.com.br>
2023-07-01 15:32:53 -0300

    Aula 4.1 parte 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,111 +1,110 @@
+import { todoController } from "@ui/controller/todo";
 import { GlobalStyles } from "@ui/theme/GlobalStyles";
+import React from "react";
+
+const bg = "/bg.jpeg"; // local image in public folder
+
+interface HomeTodo {
+  id: string;
+  content: string;
+  done: boolean;
+}
 
 function HomePage() {
+  const [todos, setTodos] = React.useState<HomeTodo[]>([]);
 
-  const bg = "/bg.jpeg" // local image in public folder
+  // Load informações quando a página é carregada
+  React.useEffect(() => {
+    todoController.get().then((res) => {
+      setTodos(res);
+    });
+  }, []);
 
   return (
     <main>
       <GlobalStyles themeName="coolGrey" />
-       <header
-         style={{
-            backgroundImage: `url('${bg}')`,
-         }}
-       >
-         <div className="typewriter">
-           <h1>O que fazer hoje?</h1>
-         </div>
-         <form>
-           <input
-             type="text"
-             placeholder="Correr, Estudar..."
-           />
-           <button
-             type="submit"
-             aria-label="Adicionar novo item"
-           >
-             +
-           </button>
-         </form>
-       </header>
- 
-       <section>
-         <form>
-           <input
-             type="text"
-             placeholder="Filtrar lista atual, ex: Dentista"
-           />
-         </form>
- 
-         <table border={1}>
-           <thead>
-             <tr>
-               <th align="left">
-                 <input type="checkbox" disabled />
-               </th>
-               <th align="left">Id</th>
-               <th align="left">Conteúdo</th>
-               <th />
-             </tr>
-           </thead>
- 
-           <tbody>
-               <tr>
-                 <td>
-                   <input
-                     type="checkbox"
-                   />
-                 </td>
-                 <td>d4f26</td>
-                 <td>
-                   Conteúdo de uma TODO
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque vero facilis obcaecati, autem aliquid eius! Consequatur eaque doloribus laudantium soluta optio odit, provident, ab voluptates doloremque voluptas recusandae aspernatur aperiam.
-                 </td>
-                 <td align="right">
-                   <button
-                     data-type="delete"
-                   >
-                     Apagar
-                   </button>
-                 </td>
-               </tr>
- 
-               <tr>
-                 <td colSpan={4} align="center" style={{ textAlign: "center" }}>
-                   Carregando...
-                 </td>
-               </tr>
- 
-               <tr>
-                 <td colSpan={4} align="center">
-                   Nenhum item encontrado
-                 </td>
-               </tr>
- 
-               <tr>
-                 <td colSpan={4} align="center" style={{ textAlign: "center" }}>
-                   <button
-                     data-type="load-more"
-                   >
-                     Carregar mais{" "}
-                     <span
-                       style={{
-                         display: "inline-block",
-                         marginLeft: "4px",
-                         fontSize: "1.2em",
-                       }}
-                     >
-                       ↓
-                     </span>
-                   </button>
-                 </td>
-               </tr>
- 
-           </tbody>
-         </table>
-       </section>
-     </main>
- )
+      <header
+        style={{
+          backgroundImage: `url('${bg}')`,
+        }}
+      >
+        <div className="typewriter">
+          <h1>O que fazer hoje?</h1>
+        </div>
+        <form>
+          <input type="text" placeholder="Correr, Estudar..." />
+          <button type="submit" aria-label="Adicionar novo item">
+            +
+          </button>
+        </form>
+      </header>
+
+      <section>
+        <form>
+          <input type="text" placeholder="Filtrar lista atual, ex: Dentista" />
+        </form>
+
+        <table border={1}>
+          <thead>
+            <tr>
+              <th align="left">
+                <input type="checkbox" disabled />
+              </th>
+              <th align="left">Id</th>
+              <th align="left">Conteúdo</th>
+              <th />
+            </tr>
+          </thead>
+
+          <tbody>
+            {todos.map((todo: any) => {
+              return (
+                <tr key={todo.id}>
+                  <td>
+                    <input type="checkbox" />
+                  </td>
+                  <td>{todo.id.substring(0, 4)}</td>
+                  <td>{todo.content}</td>
+                  <td align="right">
+                    <button data-type="delete">Apagar</button>
+                  </td>
+                </tr>
+              );
+            })}
+
+            {/* <tr>
+              <td colSpan={4} align="center" style={{ textAlign: "center" }}>
+                Carregando...
+              </td>
+            </tr> */}
+
+            {/* <tr>
+              <td colSpan={4} align="center">
+                Nenhum item encontrado
+              </td>
+            </tr> */}
+
+            {/* <tr>
+              <td colSpan={4} align="center" style={{ textAlign: "center" }}>
+                <button data-type="load-more">
+                  Carregar mais{" "}
+                  <span
+                    style={{
+                      display: "inline-block",
+                      marginLeft: "4px",
+                      fontSize: "1.2em",
+                    }}
+                  >
+                    ↓
+                  </span>
+                </button>
+              </td>
+            </tr> */}
+          </tbody>
+        </table>
+      </section>
+    </main>
+  );
 }
 
 export default HomePage;
