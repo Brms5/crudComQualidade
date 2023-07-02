0ea23686fcd29b37d4835e41695c9c442e6dbb3e
Bruno <bruno.moreira@meta.com.br>
2023-07-02 11:30:26 -0300

    Aula 4 parte 2

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -14,7 +14,7 @@ function get({
 }: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
   return fetch("/api/todos").then(async (response) => {
     const todosString = await response.text();
-    const todosFromServer = JSON.parse(todosString).todos;
+    const todosFromServer = parseTodosFromServer(JSON.parse(todosString)).todos;
     console.log("page", page);
     console.log("limit", limit);
 
@@ -42,4 +42,40 @@ interface Todo {
   content: string;
   date: Date;
   done: boolean;
+}
+
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+  if (
+    typeof responseBody === "object" &&
+    responseBody !== null &&
+    "todos" in responseBody &&
+    Array.isArray(responseBody.todos)
+  ) {
+    console.log("responseBody", responseBody.todos);
+    return {
+      todos: responseBody.todos.map((todo: unknown) => {
+        if (typeof todo !== "object" || todo === null) {
+          throw new Error("Invalid todo from server");
+        }
+
+        const { id, content, date, done } = todo as {
+          id: string;
+          content: string;
+          date: string;
+          done: string;
+        };
+
+        return {
+          id,
+          content,
+          date: new Date(date),
+          done: String(done).toLowerCase() === "true",
+        };
+      }),
+    };
+  }
+  
+  return {
+    todos: [],
+  };
 }
